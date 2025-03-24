d2ff92112151ea28d1ac7b566d64134db1d2b521
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-24 13:12:04 +0530

    multiple reducers implementation

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED'
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED'
 
 // action defined
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quanity: 1
+    payload: 1
   }
 }
 
@@ -19,12 +21,30 @@ function restockCake(quanity) {
   }
 }
 
-const initialState = {
+function orderIceCream() {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: 1
+  }
+}
+
+function restockIceCream(quanity) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: quanity
+  }
+}
+
+const initialCakeState = {
   numOfCakes: 10
 }
 
+const initialIceCreamState = {
+  numOfIceCreams: 10
+}
+
 // reducer
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch(action.type) {
     case CAKE_ORDERED:
       return {
@@ -41,18 +61,35 @@ const reducer = (state = initialState, action) => {
   }
 }
 
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch(action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams -1
+      }
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload
+      }
+    default:
+      return state
+  }
+}
+
 //store created and the responsibilities 
 const store = createStore(reducer)
 console.log('Initial State', store.getState());
 
 const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()))
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
 
-store.dispatch(restockCake(3))
-store.dispatch(restockCake(4))
-store.dispatch(restockCake(1))
+// store.dispatch(restockCake(3))
+// store.dispatch(restockCake(4))
+// store.dispatch(restockCake(1))
 
 unsubscribe()
