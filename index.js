9697b866c6f6350fc2cf05c7b84eb999b8584bd5
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-24 12:45:49 +0530

    adding another action type

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 // action defined
 function orderCake() {
   return {
     type: CAKE_ORDERED,
     quanity: 1
+  }
+}
+
+function restockCake(quanity) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: quanity
   }
 }
 
@@ -20,7 +28,13 @@ const reducer = (state = initialState, action) => {
   switch(action.type) {
     case CAKE_ORDERED:
       return {
+        ...state,
         numOfCakes: state.numOfCakes - 1
+      }
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
       }
     default:
       return state
@@ -37,6 +51,8 @@ store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 
-unsubscribe()
+store.dispatch(restockCake(3))
+store.dispatch(restockCake(4))
+store.dispatch(restockCake(1))
 
-store.dispatch(orderCake())
+unsubscribe()
