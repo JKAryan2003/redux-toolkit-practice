1e1f7d500454338e964b06f33fa0ffe1b93b2182
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-26 16:28:54 +0530

    implemented dispatch using useDispatch on buttons

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
 
 const IcecreamView = () => {
   const numOfIcecream = useSelector((state) => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of IceCream - {numOfIcecream}</h2>
-      <button>Order IceCream</button>
-      <button>Restock IceCream</button>
+      <button onClick={() => dispatch(ordered())}>Order IceCream</button>
+      <button onClick={() => dispatch(restocked(3))}>Restock IceCream</button>
     </div>
   )
 }
